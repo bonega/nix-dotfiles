fc912f63f6aaa38d4ef30462ead71e39292c8405
Andreas Liljeqvist <bonega@gmail.com>
2023-10-29 11:05:05 +0100

    add a few packages

--- home.nix
+++ home.nix
@@ -20,6 +20,9 @@
   home.packages = with pkgs; [
     rustup
     nixfmt
+    ripgrep
+    du-dust
+    bat
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
     # pkgs.hello
