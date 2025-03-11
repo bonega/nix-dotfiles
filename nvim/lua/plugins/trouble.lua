e9f689b5010c66656f9e100d8ad5f06ca020ea14
Andreas Liljeqvist <bonega@gmail.com>
2025-03-11 18:41:43 +0100

    refactor trouble filter

--- nvim/lua/plugins/trouble.lua
+++ nvim/lua/plugins/trouble.lua
@@ -1,17 +1,25 @@
+local hutch_exclude = {
+  "hutch/src/Gql/Starsky/",
+  "hutch/submodules/",
+  "hutch/node_modules/",
+  "hutch/src/Translations.elm",
+}
+
+local function should_include(item)
+  for _, substr in ipairs(hutch_exclude) do
+    if string.find(item.filename, substr, 1, true) then
+      return false
+    end
+  end
+  return true
+end
+
 return {
   {
     "folke/trouble.nvim",
     opts = {
       filter = function(items)
-        return vim.tbl_filter(function(item)
-          return not (
-            string.match(item.filename, "hutch/src/Gql/Starsky/")
-            or string.match(item.filename, "hutch/submodules/")
-            or string.match(item.filename, "hutch/node_modules/")
-            or string.match(item.filename, "hutch/src/Translations.elm")
-
-          )
-        end, items)
+        return vim.tbl_filter(should_include, items)
       end,
     },
   },
